88ee3d232f443e0994e1b0267499a02320dba72d
Aspaldikofg <aspaldiko@gmail.com>
2026-02-23 21:08:30 +0000

    File changes

--- src/pages/Pipeline.jsx
+++ src/pages/Pipeline.jsx
@@ -314,7 +314,7 @@ export default function Pipeline() {
         isOpen={showNewPatient}
         onClose={() => setShowNewPatient(false)}
         onSave={handleCreatePatient}
-        users={users}
+        users={activeResponsibles}
         currentUser={currentUser}
       />
 
