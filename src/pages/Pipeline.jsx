429da9df42677814d44b7e0f34a2bcfcf7f3422e
Aspaldikofg <aspaldiko@gmail.com>
2026-02-23 21:07:26 +0000

    File changes

--- src/pages/Pipeline.jsx
+++ src/pages/Pipeline.jsx
@@ -47,6 +47,13 @@ export default function Pipeline() {
     queryFn: () => base44.entities.User.list()
   });
 
+  const { data: responsibles = [] } = useQuery({
+    queryKey: ['responsibles'],
+    queryFn: () => base44.entities.Responsible.list('name')
+  });
+
+  const activeResponsibles = responsibles.filter(r => r.is_active);
+
   const { data: config = [] } = useQuery({
     queryKey: ['appConfig'],
     queryFn: () => base44.entities.AppConfig.list()
